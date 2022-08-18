49b888a0535097ce964f26938af3751ba9e02f80
ilhamsg7 <sinatrio20@gmail.com>
2022-08-18 21:40:59 +0700

    Copy Migration, Controller, Factory, Seeder, and Routing from v1 to v2

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -18,10 +18,18 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'date_birth',
+        'address',
+        'gender',
+        'photo_path',
+        'phone'
     ];
+
+    protected $with = ['role'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +49,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getNameAttribute() {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function getPhotoPathAttribute($value) {
+        return asset('storage/' . $value);
+    }
+
+    public function getDateBirthAttribute($value) {
+        return date('d-m-Y', strtotime($value));
+    }
+
+    public function membership() {
+        return $this->hasMany(Membership::class);
+    }
+
+    public function result() {
+        return $this->hasMany(Result::class);
+    }
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('role_id')->constrained();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('date_birth')->nullable();;
+            $table->string('address')->nullable();;
+            $table->char('gender')->nullable();;
+            $table->string('photo_path')->nullable();
+            $table->string('phone', 25)->nullable();;
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
